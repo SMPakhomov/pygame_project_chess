24f5a2d644529e36420aa72046f14234ae349344
Семён <sem.pakhomov2007@yandex.ru>
2023-01-02 22:29:01 +0300

    add Figure class and rebuild Desk class

--- chess_engine.py
+++ chess_engine.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import sys
 
 
 class Game:
@@ -25,24 +26,29 @@ class Game:
 
 
 class Desk:
-    def __init__(self, time, color=((255, 255, 255), (0, 0, 0))):
+    def __init__(self, time, color=((240, 240, 240), (100, 100, 100))):
         with open('def_desk.txt', 'r') as file:
-            self.desk = [file.readline().split() for _ in range(8)]
+            self.desk = [file.readline().split(' ') for _ in range(8)]
+            self.figures = []
+            for i in range(8):
+                tmp = []
+                for j in range(8):
+                    if self.desk[i][j].rstrip('\n') != '.':
+                        tmp.append(Figure((i, j), self.desk[i][j]))
+                    else:
+                        tmp.append(None)
+                self.figures.append(tmp)
         self.color = color  # цвет доски - кореж из двух цветов в формате rgb
         self.time = [time, time]
         self.turn = 0  # чей ход
 
     def update(self, t):
-        color = "white"
-        for i in range(8):
-            for j in range(9):
-                if j != 8:
-                    screen.fill(pygame.Color(color), pygame.Rect(62.5 * j + 20, 62.5 * i + 20, 62.5, 62.5))
-                if color == "white":
-                    color = "black"
-                else:
-                    color = "white"
+        self.draw_desk()
+        self.draw_timer(t)
+        figures.update()
+        figures.draw(screen)
 
+    def draw_timer(self, t):
         font = pygame.font.Font(None, 20)
         tm = self.time[0]
         txt = "Player 1 - " + str(int(tm)) + ':' + str(int((tm % 1) * 100))
@@ -56,12 +62,48 @@ class Desk:
         if self.time[self.turn] % 1 > 0.6:
             self.time[self.turn] -= 0.4
 
+    def draw_desk(self):
+        color = self.color[0]
+        for i in range(8):
+            for j in range(9):
+                if j != 8:
+                    screen.fill(pygame.Color(color), pygame.Rect(62.5 * j + 20, 62.5 * i + 20, 62.5, 62.5))
+                if color == self.color[0]:
+                    color = self.color[1]
+                else:
+                    color = self.color[0]
+
         font = pygame.font.Font(None, 20)
         for i in range(8):
             text = font.render(str(i + 1), 1, self.color[1])
             screen.blit(text, (5, 520 - (i * 62.5 + 42.25)))
             text = font.render(chr(i + 65), 1, self.color[1])
             screen.blit(text, ((i * 62.5 + 42.25), 520))
+
+
+class Figure(pygame.sprite.Sprite):
+    def __init__(self, pos, type):
+        super().__init__(figures)
+        self.pos = pos
+        self.type = type.strip('\n')
+        self.image = self.load_image(self.type + '.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[1] * 62.5 + 22.25
+        self.rect.y = pos[0] * 62.5 + 22.25
+
+    def load_image(self, name):
+        fullname = os.path.join('DATA', name)
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        return image
+
+
+class Pawn(Figure):
+
+    def __init__(self, *args):
+        super().__init__(*args)
 
 
 pygame.init()
